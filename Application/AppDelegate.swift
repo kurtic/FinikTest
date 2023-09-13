c23d036d02dabcd7dc2dda4745678dea2deb76e4
Diachenko Ihor <>
2023-09-13 19:41:46 +0300

    initial commit

---
+++ Application/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  FinikTest
+//
+//  Created by Diachenko Ihor on 13.09.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+}
+
